28d2db61345ba0f95291a56c225fb3661ca1e7af
Abdelrahman Sabry <52915804+TheTallDeveloper@users.noreply.github.com>
2023-02-05 19:03:01 +0200

    adding needed components

--- bakery-goods/src/app/app.module.ts
+++ bakery-goods/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './order/order.component';
+import { LocationsComponent } from './locations/locations.component';
+import { AboutComponent } from './about/about.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    OrderComponent,
+    LocationsComponent,
+    AboutComponent,
+    SignInComponent,
+    SignUpComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
